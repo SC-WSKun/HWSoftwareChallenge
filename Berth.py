f49da88743f8f54f8a2079e6b336d8682a48c94f
liujiawen-jpg <57214494+liujiawen-jpg@users.noreply.github.com>
2024-03-20 13:01:08 +0800

    Update Berth.py

--- Berth.py
+++ Berth.py
@@ -8,20 +8,20 @@ class Berth:
         self.nums = [0 for i in range(15000)]
         self.boat = None # 最快到达港口的船
         self.status = 0 #0没有船要过来，1有船正在泊位上或者将要过来
-        self.robort_arrive_time = -1 #机器人到达的时间
+        self.robot_arrive_time = -1 #机器人到达的时间
         self.all_path = {}  # 字典，(x,y)到所有点的路径，key是点的编号（number），value是(x,y)到这个点的最短路径，不连通的key不会出现在all_path中
         # 这个路径记录的是港口到点的路径，机器人要使用这个路径的话得对value进行逆转，即[::-1]
 
-    def robort_pull(self,time):#机器人放置物品到码头，time是机器人到达的时间
+    def robot_pull(self,time):#机器人放置物品到码头，time是机器人到达的时间
         self.nums[time:]+=1
-        self.robort_arrive_time = time
+        self.robot_arrive_time = time
 
-    def robort_undo(self,time=-1):
+    def robot_undo(self,time=-1):
         # -1 表示当前机器人放弃计划后，暂时没有去拿货物
-        self.nums[self.robort_arrive_time:]-=1
+        self.nums[self.robot_arrive_time:]-=1
         if(time!=-1):
             self.nums[self.time:]+=1
-        self.robort_arrive_time=time
+        self.robot_arrive_time=time
 
     
     def boat_load(self,current_time): #每帧执行
